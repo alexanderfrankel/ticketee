7bc2a0b7be5d82f4af34f7d8629f898694f3d5d8
Alexander Frankel <alexxander.frankel@gmail.com>
2014-07-12 13:06:33 -0400

    Add the ability to view a list of all projects

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature "Viewing projects" do
 	scenario "Listing all projects" do
-		project = create(:project, name: "TextMate2")
+		project = create(:project, name: "TextMate 2")
 		visit '/'
 		click_link 'TextMate 2'
 		expect(page.current_url).to eql(project_url(project))
