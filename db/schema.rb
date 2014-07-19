103ce1db8b3c2bcb8d894bde8a5978fac2fd8c14
Alexander Frankel <alexxander.frankel@gmail.com>
2014-07-19 13:47:45 -0400

    Implement user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -33,4 +33,12 @@ ActiveRecord::Schema.define(version: 20140719170802) do
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id", using: :btree
 
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
